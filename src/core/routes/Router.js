3e37922f2adc912527354c3dbb1255f83b722831
Jss2k <cliffsclifus@icloud.com>
2020-09-16 00:55:28 +0300

    implement DIP

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,7 +1,6 @@
+import {Loader} from '../../components/Loader'
 import {$} from '../dom'
 import {ActiveRoute} from './ActiveRoute'
-// import { Page } from '../Page'
-// import {ActiveRoute} from './ActiveRoute'
 
 export class Router {
   constructor(selector, routes) {
@@ -11,6 +10,8 @@ export class Router {
 
     this.$placeholder = $(selector)
     this.routes = routes
+
+    this.loader = new Loader()
 
     this.page = null
 
@@ -24,12 +25,12 @@ export class Router {
     this.changePageHandler()
   }
 
-  changePageHandler() {
+  async changePageHandler() {
     if (this.page) {
       this.page.destroy()
     }
 
-    this.$placeholder.clear()
+    this.$placeholder.clear().append(this.loader)
 
     const Page = ActiveRoute.path.includes('excel')
     ? this.routes.excel
@@ -37,7 +38,9 @@ export class Router {
 
     this.page = new Page(ActiveRoute.param)
 
-    this.$placeholder.append(this.page.getRoot())
+    const root = await this.page.getRoot()
+
+    this.$placeholder.clear().append(root)
 
     this.page.afterRender()
   }
